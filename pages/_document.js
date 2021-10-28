9be8b953b3682e0a6bcb83505a95d4b0a7e47916
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-10-28 17:57:49 -0500

    custom info window when clicking on a place.

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="lenwell cms" content="App" />
+            <script
+                async
+                src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAP_API}&callback=initMap&libraries=places`}
+            >
+            </script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,58 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { useEffect } from 'react';
+const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
 
 export default function Home() {
+
+  useEffect(() => {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 29.718557455282742, lng: -95.51367567423155},
+      zoom: 15,
+    });
+
+    const marker = new google.maps.Marker({
+      position: {
+        lat: 29.7190437, 
+        lng: -95.5117378
+      },
+      map: map,
+      animation: google.maps.Animation.DROP
+    })
+
+    const infoWindow = new google.maps.InfoWindow();
+    infoWindow.setContent(document.getElementById("info-window-content"));
+
+    //is this click event clicking on a place
+    const isIconMouseEvent = (e) => placeId in e;
+
+    map.addListener("click", (event) => {
+      console.log("you clicked " + event.latLng);
+      if(event.placeId) {
+        console.log("you clicked on place: " + event.placeId);
+        event.stop();
+        infoWindow.close();
+        infoWindow.setPosition(event.latLng);
+        infoWindow.open(map);
+      }
+    });
+
+    google.maps.event.addDomListener(marker, 'click', function(){
+      console.log(marker);
+    });
+
+
+
+  }, []);
+
   return (
     <div className={styles.container}>
-      Map app
+        <div id="map" style={{
+          width: "100VW",
+          height: "100VH"
+        }}>
+          <div id="info-window-content">Hi</div>
+        </div>
     </div>
   )
 }
