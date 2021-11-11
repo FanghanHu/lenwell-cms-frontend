5bc9c39adfd3ddb47cc04b58839ecdce9277a43e
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-11-11 17:33:43 -0600

    only logged in user can use the  main page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import { useRouter } from 'next/dist/client/router';
 import { useEffect, useState } from 'react';
 import GoogleMap from '../components/google-map';
 import Header from "../components/header";
 import { withSession } from '../middlewares/session';
 
 export default function Home({user}) {
+  const router = useRouter();
+
+  if(!user) {
+    router.push('/login');
+    return <div>You need to login first</div>
+  }
+
   useEffect(() => {
     //expose backend url to window object
     window.BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
