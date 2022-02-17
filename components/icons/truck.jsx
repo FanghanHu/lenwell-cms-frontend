32fd362c1ff59e13985a9033c6a3d43b4b2caac6
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-02-17 11:13:08 -0600

    adjust icons

--- components/icons/truck.jsx
+++ components/icons/truck.jsx
@@ -2,7 +2,7 @@ export default function TruckIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      height="1em"
+      width="24px"
       fill="none"
       stroke="currentColor"
       viewBox="0 0 24 24"
