13b98a45f91049846ad52c2aa0db27165b27d9b2
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-03-16 15:52:22 -0500

    show chat messages

--- components/chat-box/index.jsx
+++ components/chat-box/index.jsx
@@ -5,7 +5,8 @@ import Chat from "../icons/chat";
 import { useRef, useState } from "react";
 import { toast } from "react-nextjs-toast";
 import axios from "axios";
-import { useUser } from "../../context/user-context";
+import { useUser, useUsers } from "../../context/user-context";
+import Markdown from "marked-react";
 
 export default function ChatBox({
 	isActive,
@@ -16,6 +17,7 @@ export default function ChatBox({
 	const [input, setInput] = useState("");
 	const imageRef = useRef(null);
 	const user = useUser();
+	const users = useUsers();
 	const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 	function sendImage(e) {
@@ -59,7 +61,7 @@ export default function ChatBox({
 	}
 
 	return (
-		<div className={`${style.container} ${isActive ? style.active : ""}`}>
+		<div className={`${style.container} ${isActive && location?.messages ? style.active : ""}`}>
 			<div className="d-flex justify-content-end">
 				<div
 					className="px-2"
@@ -70,47 +72,74 @@ export default function ChatBox({
 					<Cross />
 				</div>
 			</div>
-			<div className="h4 text-center text-muted m-1">{location?.["display_name"]?.length > 0 ? location["display_name"] : location?.name}</div>
-			<div className={style["chat-container"]}></div>
-			<div className="form-group m-2">
-				<div className="h5">Message:</div>
-				<textarea
-					value={input}
-					onChange={(e) => {
-						setInput(e.target.value);
-					}}
-					onKeyPress={(e) => {
-						//send chat when ctrl+enter is pressed while focused on this element
-						if (e.code === "Enter" && e.ctrlKey) {
-							e.preventDefault();
-							sendChat();
-						}
-					}}
-					className="form-control"
-					style={{ resize: "none" }}
-					rows={5}
-					spellCheck={false}
-				></textarea>
+			<div className="h4 text-center text-muted m-1">
+				{location?.["display_name"]?.length > 0
+					? location["display_name"]
+					: location?.name}
 			</div>
-			<div className="d-flex justify-content-end m-2">
-				<input
-					type="file"
-					ref={imageRef}
-					accept="image/"
-					className="d-none"
-					onChange={sendImage}
-				/>
-				<button
-					className="btn btn-warning mx-1 px-3"
-					onClick={() => {
-						imageRef.current.click();
-					}}
-				>
-					<Image />
-				</button>
-				<button className="btn btn-success mx-1 px-3" onClick={sendChat}>
-					<Chat />
-				</button>
+			<div className={style["chat-container"]}>
+				{location?.messages?.map((message, index) => {
+					const sender = users.find((el) => el.id === message.sender);
+					const messageTime = new Date(message.created_at);
+					return (
+						<div
+							className={`${style["chat-message"]} ${
+								sender?.id === user.id ? style["self"] : ""
+							}`}
+							key={index}
+						>
+							<div className="d-flex justify-content-between">
+								<div className="fw-bold">{sender?.name}:</div>
+								<div className="text-muted">{`${messageTime.toLocaleDateString()} ${messageTime.toLocaleTimeString()}`}</div>
+							</div>
+							<div className={style["chat-bubble"]}>
+								<Markdown>{message.text}</Markdown>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+			<div className={style["chat-control"]}>
+				<div className="form-group m-2">
+					<div className="h5">Message:</div>
+					<textarea
+						value={input}
+						onChange={(e) => {
+							setInput(e.target.value);
+						}}
+						onKeyPress={(e) => {
+							//send chat when ctrl+enter is pressed while focused on this element
+							if (e.code === "Enter" && e.ctrlKey) {
+								e.preventDefault();
+								sendChat();
+							}
+						}}
+						className="form-control"
+						style={{ resize: "none" }}
+						rows={5}
+						spellCheck={false}
+					></textarea>
+				</div>
+				<div className="d-flex justify-content-end m-2">
+					<input
+						type="file"
+						ref={imageRef}
+						accept="image/"
+						className="d-none"
+						onChange={sendImage}
+					/>
+					<button
+						className="btn btn-warning mx-1 px-3"
+						onClick={() => {
+							imageRef.current.click();
+						}}
+					>
+						<Image />
+					</button>
+					<button className="btn btn-success mx-1 px-3" onClick={sendChat}>
+						<Chat />
+					</button>
+				</div>
 			</div>
 		</div>
 	);
